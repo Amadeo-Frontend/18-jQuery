0c34fa524d0a04b4df6cefb8ff83fd8b7bbb25b5
Amadeo Bon <amadeosph@outlook.com>
2022-07-10 02:15:08 -0300

    jQuery

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# exercicio-18-jQuery
+jQuery
+##
+"write less, do more"
+
+    Biblioteca de JavaScript
+    Carregamento rápido
+    Converte para JS (ECMAScript) padrão
+    Crossbrowser: funciona em diversos navegadores
+    Escrever um código para todos os navegadores
+    Rápida captura e transmissão de dados
+    Manipula o DOM
+    Facilita a consulta (query) a elementos
+    Extensível com plugins
+    Instalação: https://jquery.com/download/
+##
+Seletores
+##
+Simples
+
+    $('h4') // tag
+    $('.featured-item') // class
+    $('#featured') // id
+##
+Compostos
+
+    $('h4, h6')
+
+    $('div h4')
+
---
+++ custom.js
@@ -0,0 +1,57 @@
+// instancia jquery e evita conflitos
+// jQuery( function($){
+    $(document).ready(function(){
+
+        $('.owl-carousel').owlCarousel();
+    
+        let titulos = $('h4') // tag
+       
+        let itens = $('.featured-item') // class
+        
+        let destaques = $('#featured') // id
+    
+        console.log(titulos.first());
+    
+        // Configuração de produtos
+    
+        $('.featured-item a').addClass('btn btn-dark stretch-link');
+    
+        $('.featured-item:first h4').append('<span class="badge bg-secondary">Novo</span>')
+        // $('.featured-item:first h4').start('<span class="badge bg-secondary">Novo</span>')
+        // $('.featured-item:first h4').html('<span class="badge bg-secondary">Novo</span>')
+        // $('.featured-item:first h4').addClass('active')
+        // $('.featured-item:first h4').removeClass('active')
+        // $('.featured-item:first h4').toggleClass('active')
+        // $('.featured-item:first h4').hide()
+        // $('.featured-item:first h4').show()
+        // $('.featured-item:first h4').fadeIn(2000)
+        // $('.featured-item:first h4').fadeOut()
+        //  $('.featured-item:first h4').css('color', '#f00')
+         
+         $('.featured-item h4').dblclick( function(){
+    
+            $(this).css({
+                'color': '#f00',
+                'background': '#ff0',
+                'font-weight': '100',
+            });
+    
+         });
+    
+         /*
+          * Manipulação de eventos
+          */
+         $('.featured-item a').on('blur', function(event){
+    
+            event.preventDefault();
+    
+            alert('Produto esgotado');
+    
+         })
+    
+    })
+    const myCarouselElement = document.querySelector('#carouselControls')
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 3000,
+  
+})
